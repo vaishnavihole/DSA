d4ded94a4359d3665a230b710480f58a8cb4c71b
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-12 12:44:43 +0530

    using loop

---
+++ Array/array.cpp
@@ -0,0 +1,41 @@
+ // Print elements in array
+
+//   #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//    int marks[4];
+   
+//    marks[0] = 5;
+//    marks[1] = 10;
+//    marks[2] = 15;
+//    marks[3] = 20;
+   
+//    cout<<marks[0]<<endl;
+//    cout<<marks[1]<<endl;
+//    cout<<marks[2]<<endl;
+//    cout<<marks[3]<<endl;
+//    return 0;
+// }
+
+
+// ###### Using Loop #######
+
+// int main()
+// {
+//    int marks[4];
+   
+//    for(int i=0; i<4; i++)
+//    {
+//       cout<<"Enter value for index"<<i<<":";
+//       cin>>marks[i];
+//    }
+   
+//    for (int i=0; i<4; i++)
+//    {
+//       cout<<marks[i]<<endl;
+//    }
+//    return 0;
+// }
+
