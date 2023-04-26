0c0bca6a4f80e0ddef91eea03a251ead29d639f9
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-26 09:00:27 +0530

    Convert String Small to capaital

--- Strings/String.cpp
+++ Strings/String.cpp
@@ -43,3 +43,23 @@
 // }
 
 
+// Convert String Small to capaital 
+// #include<iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//    string  str = "india";
+   
+//    for(int i=0; i<str.length(); i++)
+//    {
+//        if(str[i]>='a' && str[i]<='z')
+//        str[i] = str[i] - 32;
+//    }
+   
+//    cout<<"Capital word is: "<<str;
+// }
+
+
+
